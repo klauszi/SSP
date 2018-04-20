67603b10d05398cea57ddb5e4b17176b5f469def
Long Huynh Huu <long@huynhgia.de>
2018-04-20 16:30:00 +0200

    Fehlende Dateien für Commit 2778c15025258fd243b7234791cb7d6c40a040f5

--- Exercise1/julia-implementation/02-block-processing.jl
+++ Exercise1/julia-implementation/02-block-processing.jl
@@ -10,18 +10,17 @@ x1 = (1:length(y1))/fs1
 x2 = (1:length(y2))/fs2
 
 my_windowing = function(v_signal, sampling_rate, frame_length, frame_shift)
-    sample_count = length(v_signal)
-    window_size = Int(floor(frame_length * sampling_rate))
-    shift_size = Int(floor(frame_shift * sampling_rate))
-    shift_count = Int(floor((sample_count - (window_size - shift_size))/shift_size))
+    samples_per_frame = Int(floor(frame_length * sampling_rate))
+    samples_per_shift = Int(floor(frame_shift * sampling_rate))
+    shift_count = Int(floor((length(v_signal) - (samples_per_frame - samples_per_shift))/samples_per_shift))
 
-    m_frames = zeros(shift_size, shift_count)
+    m_frames = zeros(samples_per_shift, shift_count)
     for j in 1:shift_count
-        for i in 1:shift_size
-            m_frames[i,j] = v_signal[shift_size*(j-1)+i]
+        for i in 1:samples_per_shift
+            m_frames[i,j] = v_signal[samples_per_shift*(j-1)+i]
         end
     end
-    v_time_frame = frame_shift * ((1:shift_count)-1) + frame_length/2
+    v_time_frame = frame_length/2 + frame_shift * (0:(shift_count-1))
 
     (m_frames, v_time_frame)
 end
--- Exercise1/julia-implementation/README.md
+++ Exercise1/julia-implementation/README.md
@@ -1,4 +1,5 @@
 Hier ist Code für Aufgaben 2 und 3 in Julia, präsentiert als [https://github.com/JuliaLang/IJulia.jl][IJulia Notebook].
+"Exercise 1.html" ist der Html-Export von "Exercise 1.ipynb" und könnte out-of-date sein :)
 Work in Progress.
 
 
